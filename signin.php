9253f2a4f1be431a4dfec1629034f872147bfcd1
dvhky <dvhky.vu@gmail.com>
2023-11-22 22:01:06 +0700

    Fix validation using css

--- signin.php
+++ signin.php
@@ -4,8 +4,7 @@ session_start();
 
 $username = $password = $confirm_password = "";
 $username_err = $password_err = $confirm_password_err = "";
-echo $_SESSION['uuid'];
-echo $_POST['uuid'];
+
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && $_POST['uuid'] == $_SESSION['uuid']) {
     $username = $_POST['username'];
@@ -15,8 +14,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && $_POST['uuid'] == $_SESSION['uuid'])
     $result = $conn->query($stmt);
     $row = $result->fetch_assoc();
 
-    echo $row['password'];
-
     if($row['username'] == $username && $password == $row['password']) {
       //assume username is unique
       $_SESSION['username'] = $username;
@@ -24,8 +21,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && $_POST['uuid'] == $_SESSION['uuid'])
 
 }
 
-?>
-
+?>  
 
 <!DOCTYPE html>
 <html lang="en">
@@ -38,35 +34,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && $_POST['uuid'] == $_SESSION['uuid'])
 <body>
     <div class="flex flex-col items-center justify-center h-screen">
     <div class="w-full max-w-xs">
-  <form id='form' action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" method="post" onsubmit="onSubmit(event)">
-  <?php $uuid = rand(); $_SESSION['uuid'] = $uuid;?>
+  <form novalidate  id='form' action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" class="group bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 " method="post" onsubmit="onSubmit(event)">
+  <?php $uuid = rand(); $_SESSION['uuid'] = $uuid?>
   <input type="hidden" value="<?php echo $uuid; ?>" id="uuid" name="uuid" />
-  <div class="mb-4">
+  <div class="mb-1">
       <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
         Logo here
       </label>
     </div>
-    <div class="mb-4">
+    <div class="mb-1">
       <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
         Username
       </label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight mb-3 focus:outline-none focus:shadow-outline" name="username" id="username" type="text" placeholder="Username">
-			<p class="hidden text-red-500 text-xs italic"> Username not valid.</p>
+      <input class="peer invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight mb-3 focus:outline-none focus:shadow-outline" name="username" id="username" type="text" placeholder="Username" pattern="[a-zA-Z0-9]{7,}$" required>
+      <p class="invisible peer-placeholder-shown:!invisible peer-invalid:visible text-red-500 text-xs italic"> Username not valid.</p>
     </div>
-    <div class="mb-6">
+    <div class="mb-1">
       <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
         Password
       </label>
-      <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
-      <p class="hidden text-red-500 text-xs italic"> Password not valid.</p>
+      <input class="peer shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500" id="password" type="password" placeholder="******************"  pattern=".{7,}" required>
+      <p class="invisible peer-placeholder-shown:!invisible peer-invalid:visible text-red-500 text-xs italic"> Password not valid.</p>
     </div>
-    <div class="flex items-center justify-between">
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+    <div class="mb-4 flex items-center justify-between">
+      <button class="group-invalid:pointer-events-none group-invalid:opacity-30 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
         Sign In
       </button>
-      <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
-        Forgot Password?
-      </a>
+    </div>
+    <div class="text-sm">
+      Not registered?
+      <a href="signup.php" class="hover:underline text-[#3B82F6]"> Create account  </a> 
     </div>
   </form>
   <p class="text-center text-gray-500 text-xs">
@@ -75,7 +72,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && $_POST['uuid'] == $_SESSION['uuid'])
 </div>
     </div>
 </body>
-<script src="https://cdn.tailwindcss.com"></script>
 <script src="https://unpkg.com/validator@latest/validator.min.js"></script>
-<script src="./js/signin.validate.js"></script>
 </html>
--- signup.php
+++ signup.php
@@ -14,27 +14,30 @@
 <body>
     <div class="flex flex-col items-center justify-center h-screen">
     <div class="w-full max-w-xs">
-  <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-  <div class="mb-4">
+  <form novalidate  id='form' action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" class="group bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 " method="post" onsubmit="onSubmit(event)">
+  <?php $uuid = rand(); $_SESSION['uuid'] = $uuid?>
+  <input type="hidden" value="<?php echo $uuid; ?>" id="uuid" name="uuid" />
+  <div class="mb-1">
       <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
         Logo here
       </label>
     </div>
-    <div class="mb-4">
+    <div class="mb-1">
       <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
         Username
       </label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
+      <input class="peer invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight mb-3 focus:outline-none focus:shadow-outline" name="username" id="username" type="text" placeholder="Username" pattern="[a-zA-Z0-9]{7,}$" required>
+      <p class="invisible peer-placeholder-shown:!invisible peer-invalid:visible text-red-500 text-xs italic"> Username not valid.</p>
     </div>
-    <div class="mb-6">
+    <div class="mb-1">
       <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
         Password
       </label>
-      <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
-      <p class="text-red-500 text-xs italic">Please choose a password.</p>
+      <input class="peer shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500" id="password" type="password" placeholder="******************"  pattern=".{7,}" required>
+      <p class="invisible peer-placeholder-shown:!invisible peer-invalid:visible text-red-500 text-xs italic"> Password not valid.</p>
     </div>
-    <div class="flex items-center justify-center">
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+    <div class="mb-4 flex items-center justify-center">
+      <button class="group-invalid:pointer-events-none group-invalid:opacity-30 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
         Sign Up
       </button>
     </div>
